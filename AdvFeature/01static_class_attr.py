40387c2d249698b76796adbc08202c7d45c25946
duzhaoqi <linuxbugs@163.com>
2019-10-17 10:57:13 +0800

    类属性与实例属性

--- AdvFeature/01static_class_attr.py
+++ AdvFeature/01static_class_attr.py
@@ -1,7 +1,28 @@
+"""
+类与对象区别
+类实例与对象实例区别
+"""
+class AI(object):
+    """
+    AI父类
+    """
+    # AI有是否存活类属性
+    isAlive = False
+    def __init__(self, _hp, _mp):
+        # AI 有血量和魔法值实例属性
+        self.hp = _hp
+        self.mp = _mp
 
-class Ai:
-    def __init__(self) -> None:
-        super().__init__()
 
-    def show(self):
-        print("Hello")
+if __name__ == "__main__":
+    # 实例a1、a2中hp、mp属性不同
+    a1 = AI(100, 50)
+    a2 = AI(60, 120)
+
+    print(a1.mp, a2.mp)
+    print(a1.isAlive, a2.isAlive)
+
+    # 实例可以调类属性
+    print(AI.isAlive)
+    # 类名不可以调实例属性错误
+    #print(AI.mp)
