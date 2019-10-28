8bb374ca3efb6487bcc2a8038d483e9eaf70be79
duzhaoqi <linuxbugs@163.com>
2019-10-28 11:26:21 +0800

    rrr

--- AdvFeature/07metaClass.py
+++ AdvFeature/07metaClass.py
@@ -44,3 +44,42 @@ Human.sleep()
 
 print("+="* 20)
 
+#==============================
+
+def move(self):
+    print("Move")
+
+AI = type("AI",(object,),{"move":move})
+
+#查询类内部的属性和方法(列表形式)
+print(dir(AI))
+#字典形式查询类内部的属性和方法(或者使用 dict() )
+print(AI.__dict__)
+
+"""
+Python在创建NPC类时先不在内存中创建、首先查看NPC中有__metaclass__这个属性吗
+如果有，Python会通过__metaclass__创建一个名字为NPC的类(对象)
+如果Python没有找到__metaclass__，它会继续在AI（父类）中寻找__metaclass__属性，并尝试做和前面同样的操作。
+如果Python在任何父类中都找不到__metaclass__，它就会在模块层次中去寻找__metaclass__，并尝试做同样的操作。
+如果还是找不到__metaclass__,Python就会用内置的type来创建这个类对象
+"""
+
+#利用元类创建新类
+## 要求: 属性名为 小写类名+小写属性名
+
+#创建 类创建函数
+def designClass(currentclass,parentclassname,attrdict):
+    newAttrDict = {}
+    for  k, v in attrdict.items():
+        if not k.startswith("__"):
+            newAttrDict[currentclass.lower()+k.lower()] = v
+
+    return  type(currentclass,(),newAttrDict)
+
+class NPC(metaclass=designClass): #使用metaclass创建类
+    speed = 10
+    name = "Du"
+
+
+print(hasattr(NPC,"speed")) #NPC类中不存在speed属性
+print(hasattr(NPC,'npcspeed')) #NPC类中存在npcspeed属性
