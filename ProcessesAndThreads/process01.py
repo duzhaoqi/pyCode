f5214dcfb8d9cc0ced13d6779f8aa3a6c65227a2
duzhaoqi <linuxbugs@163.com>
2019-10-29 18:14:36 +0800

    rrr

--- ProcessesAndThreads/process01.py
+++ ProcessesAndThreads/process01.py
@@ -1,4 +1,34 @@
 import os
+from multiprocessing import Process
 
-pid = os.fork()
-print(pid)
+# pid = os.fork()
+# print("do")
+# if pid == 0:
+#     print("child process pid %d" % pid)
+# else:
+#     print("parent process pid %d" % pid)
+
+# pid = os.fork()
+# print("do")
+# if pid == 0:
+#     # print("child process id %d" % pid)
+#     print("current child process pid %d parent pid %d" % (os.getpid(), os.getppid()))
+# else:
+#     # print("parent process id %d" % pid)
+#     print("current parent process pid %d child pid %d" % (os.getpid(), pid))
+
+num = 10
+
+returncode = os.fork()
+print(num)
+if returncode == 0:
+    #child process
+    num = 100
+else:
+    #parent process
+    num = 300
+
+if returncode == 0:
+    print("child",num)
+else:
+    print("parent",num)
