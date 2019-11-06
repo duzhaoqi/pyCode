28d728dc1ad8d3c63540a1bcd5fdf8209143d975
duzhaoqi <linuxbugs@163.com>
2019-11-06 18:28:28 +0800

    update

---
+++ Networks/TcpSocket02.py
@@ -0,0 +1,28 @@
+import socket
+
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect(("127.0.0.1",10001))
+
+# def sendMessage():
+#     while True:
+#         info = input(">")
+#         try:
+#             client.send(info.encode("UTF-8"))
+#         except Exception:
+#             client.close()
+#             print("end")
+# def getinfo():
+#     while True:
+#         get_info = client.recv(1024*10).decode("UTF-8")
+#         print("back Message: ",get_info)
+#
+# sendMessage()
+# getinfo()
+
+
+info = input(">")
+client.send(info.encode("UTF-8"))
+get_info = client.recv(1024*10).decode("UTF-8")
+print("back Message: ",get_info)
+
+client.close()
