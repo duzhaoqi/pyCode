d567987db7cd1812976fc400a51451d716cbfe64
duzhaoqi <linuxbugs@163.com>
2019-11-07 18:20:19 +0800

    update

--- Networks/TcpSocket01.py
+++ Networks/TcpSocket01.py
@@ -18,24 +18,41 @@ def tsend(threads):
             if i[1] == int(a):
                 i[0].send(mess.encode("UTF-8"))
 
+# def tsend2(client):
+#     while True:
+#         try:
+#             if client._closed:
+#                 break
+#             else:
+#                 mess = input("message:")
+#                 client.send(mess.encode("UTF-8"))
+#         except Exception:
+#             break
+
 def recvInfo(cli):
     while True:
-        """
-        recv 方法和accept一样会进行阻塞,需要一直接受信息需要死循环
-        """
-        get = cli.recv(buffer).decode("UTF-8")
-        if not get:
+       # recv 方法和accept一样会进行阻塞,需要一直接受信息需要死循环
+        try:
+            get = cli.recv(buffer).decode("UTF-8")
+            if not get:
+                cli.close()
+                break
+            else:
+                print(get)
+        except Exception:
             break
-        else:
-            print(get)
 
 def acceptThread():
     while True:
-        cli1, addr1= srv01.accept()
-        print("...connection from :",addr1[0],":",addr1[1])
-        threads.append((cli1,addr1[1]))
-        recvs = Thread(target=recvInfo,args=(cli1,))
-        recvs.start()
+        try:
+            cli1, addr1= srv01.accept()
+            print("...connection from :",addr1[0],":",addr1[1])
+            threads.append((cli1,addr1[1]))
+            recvs = Thread(target=recvInfo,args=(cli1,))
+            recvs.start()
+        except Exception:
+            break
+
 
 def main():
     tcpget = Thread(target=acceptThread)
